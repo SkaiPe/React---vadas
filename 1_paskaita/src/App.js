344fb3c2f8ff75be5168974feed71614aacb85fc
Skaidre Petraviciene <skaidreju@gmail.com>
2023-05-17 22:45:02 +0300

    klaida

--- 1_paskaita/src/App.js
+++ 1_paskaita/src/App.js
@@ -4,29 +4,27 @@ import "./App.css";
 
 //apibrėžiame komponento pavadinimą.Visada didžiąja raidefunction App() {
 //geroji praktika: failo pavadinimas toks kaip komponento
-function App {
+function App() {
   const name = "Skaidre";
-  function getName(){
+  function getName() {
     return "Ona";
   }
-  const getSurname(){
+  const getSurname = () => {
     return "Juokutis";
   };
   const surname = getSurname();
   return (
-     <div>
-       <h1>Skaidre</h1>
-       <h2>{name}</h2>
-       <h2>{getName()}</h2>
-       <h2>{getSurname()}</h2>
-       <h2>{surname}</h2>
-       <FuncButton title="Click me" />
-       <FuncButton title="Spausk mane" />
-       <FuncButton title="Čia nespausk"/>
+    <div>
+      <h1>Skaidre</h1>
+      <h2>{name}</h2>
+      <h2>{getName()}</h2>
+      <h2>{getSurname()}</h2>
+      <h2>{surname}</h2>
+      <FuncButton title="Click me" />
+      <FuncButton title="Spausk mane" />
+      <FuncButton title="Čia nespausk" />
     </div>
   );
 }
-
-
 
 export default App;
