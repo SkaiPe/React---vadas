66dfa557d5929920afc82b3af84647c8ee4bdf2c
Skaidre Petraviciene <skaidreju@gmail.com>
2023-05-28 22:12:33 +0300

    Golfo kalendorius -pakeista pav. vieta - aplankas

--- 4_paskaita/src/App.js
+++ 4_paskaita/src/App.js
@@ -1,19 +1,29 @@
-import "./App.css";
 import Product from "./components/Product/Product";
+import calendar from "./images/calendar.jpg";
 
-function App() {
+const App = () => {
   return (
     <div>
       <Product
-        image="golf.png"
-        title="VW Golf ktūt tačka"
-        price={1900.99}
-        description="Važiuok ir džiaukis"
-        availability="In Stock"
-        category="Electronics"
+        image={calendar}
+        title="Volkswagen GOLF MK2 2022 Calendar"
+        price={10}
+        description={
+          <span>
+            <div>12 unique designs</div>
+            <div>All pieces are printed on heavyweight 200 gsm art paper.</div>
+            <div>Limited to 50 pieces</div>
+            <div>
+              <div>One size:</div>
+              11.7x16.5 inch/ (297x420 mm)
+            </div>
+          </span>
+        }
+        availability={10}
+        category="Calendars"
       />
     </div>
   );
-}
+};
 
 export default App;
