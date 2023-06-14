36909ffa223a946001c9021bc1511682c7041ea9
Skaidre Petraviciene <skaidreju@gmail.com>
2023-06-14 16:55:44 +0300

    Pakeista, bet neveikia

--- 15_pask/src/components/Button/Button.jsx
+++ 15_pask/src/components/Button/Button.jsx
@@ -1,7 +1,8 @@
 import PropTypes from "prop-types";
-import "./Button.css";
+import "./Button.scss";
 
 const Button = ({ className, children, ...props }) => {
+  //styled-button outlined
   return (
     <button className={`styled-button ${className}`} {...props}>
       {children}
@@ -12,10 +13,12 @@ const Button = ({ className, children, ...props }) => {
 Button.propTypes = {
   className: PropTypes.string,
   children: PropTypes.node.isRequired,
+  variant: PropTypes.oneOf(["contained", "outlined"]),
 };
 
 Button.defaultProps = {
   className: "",
+  variant: "contained",
 };
 
 export default Button;
