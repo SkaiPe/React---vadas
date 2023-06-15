f25b3df99d574e59cdb1567de96a919c49c0baf7
Skaidre Petraviciene <skaidreju@gmail.com>
2023-06-15 21:14:41 +0300

    Veikia projektų pridėjimas

--- 0_projectux/src/components/Button/Button.jsx
+++ 0_projectux/src/components/Button/Button.jsx
@@ -1,5 +1,4 @@
 import PropTypes from "prop-types";
-// import { Link } from 'react-router-dom';
 import "./Button.scss";
 
 const Button = ({ className, children, variant, ...props }) => {
@@ -15,11 +14,15 @@ Button.propTypes = {
   className: PropTypes.string,
   children: PropTypes.node.isRequired,
   variant: PropTypes.oneOf(["contained", "outlined"]),
+  //tiesiog įdedam
+  color: PropTypes.oneOf(["", "error", "success"]),
 };
 
 Button.defaultProps = {
   className: "",
   variant: "contained",
+  //nurodom spalvą erorui
+  color: "",
 };
 
 export default Button;
