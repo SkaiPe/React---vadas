7b24baf63c7eb70475a85a95641b890d66251609
Skaidre Petraviciene <skaidreju@gmail.com>
2023-06-19 22:04:43 +0300

    Stiliai

---
+++ 16_style/vite-project/src/components/Card/style.js
@@ -0,0 +1,15 @@
+import { styled } from "styled-components";
+
+export const StyledCardWrapper = styled.div``;
+
+export const StyledCardHeaderContainer = styled.div``;
+
+export const StyledCardHeader = styled.h5``;
+
+export const StyledDivider = styled.div``;
+
+export const StyledCardContentContainer = styled.div``;
+
+export const StyledCardContentHeader = styled.h4``;
+
+export const StyledCardContentParagraph = styled.p``;
